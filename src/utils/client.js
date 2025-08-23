cdcab301b350037ce7547f98d3069f247df5ed33
mirzam-muhammad <mirzam.muhammad@gmail.com>
2025-08-23 19:07:08 +0800

    PERTAMA KALI

---
+++ src/utils/client.js
@@ -0,0 +1,5 @@
+// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../generated/prisma/client/index.js"
+const prisma = new PrismaClient();
+
+export default prisma;
